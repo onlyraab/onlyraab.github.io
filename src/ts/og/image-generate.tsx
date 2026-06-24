69b2c282f565d86c3abb939e3652c20a03b601a3
derRaab <github.com@derraab.com>
2026-06-24 10:50:57 +0200

    Update image-generate.tsx

--- src/ts/og/image-generate.tsx
+++ src/ts/og/image-generate.tsx
@@ -1,68 +1,19 @@
 
 
 import { ImageResponse } from "@vercel/og";
-import { mkdir, readFile, writeFile } from "fs/promises";
+import { writeFile } from "fs/promises";
 import { DateEntry } from "../types/DateEntry";
 
 const fullImageWidth = 1200;
 const fullImageHeight = 630;
 const squareImageWidth = fullImageHeight;
 const squareImageHeight = fullImageHeight;
-const squareImageLeft = (fullImageWidth - squareImageWidth) / 2;
-const squareImageRight = squareImageLeft + squareImageWidth;
-const paddingImageWidth = (fullImageWidth - squareImageWidth) / 2;
 const yellow = "#ff0";
 const pink = "#f0f";
-const black = "#000";
 const backgroundColors = [yellow, pink] as const;
-const onBackgroundColors = [black, black] as const;
 const colorCount = backgroundColors.length;
 
 
-let _interFont: Buffer<ArrayBufferLike> | null = null;
-
-const getInterFont = async () => {
-  if (!_interFont) {
-    _interFont = await readFile("src/fonts/Inter/Inter-VariableFont_opsz,wght.ttf");
-  }
-  return _interFont;
-}
-
-const ImageChar = ({ char, colorIndex }: { char: string, colorIndex: number }) => {
-  const index = colorIndex % colorCount;
-  const color = onBackgroundColors[index];
-  const backgroundColor = backgroundColors[index];
-  if (char === " ") {
-    char = "\u00A0";
-  }
-  return (<span style={{ color, backgroundColor, minWidth: "2px" }}>{char}</span>);
-}
-
-const ImageBg = ({minWidth, maxWidth, minHeight, maxHeight, color, count}: {minWidth: number, maxWidth: number, minHeight: number, maxHeight: number, color: string, count: number}) => {
-  const elements = [];
-  for (let i = 0; i < count; i++) {
-    const width = Math.floor(Math.random() * (maxWidth - minWidth)) + minWidth;
-    const height = Math.floor(Math.random() * (maxHeight - minHeight)) + minHeight;
-    const left = Math.floor(Math.random() * (fullImageWidth - width));
-    const top = Math.floor(Math.random() * (fullImageHeight - height));
-    elements.push(
-      <div key={i} style={{display: "flex", position: "absolute", left, top, width, height, backgroundColor: color }} />
-    );
-  }
-
-  return (
-    <div
-      style={{
-        width: `${fullImageWidth}px`,
-        height: `${fullImageHeight}px`,
-        position: "absolute",
-        display: "flex",
-      }}
-    >
-      {elements}
-    </div>
-  );
-}
 
 const gap = 18;
 const minSize = gap;
@@ -95,46 +46,6 @@ const ImageElements = ( {left, top, width, height, colorIndex}: {left: number, t
     <ImageElements left={firstChildLeft} top={firstChildTop} width={childWidth} height={childHeight} colorIndex={colorIndex + 1} />
     <ImageElements left={secondChildLeft} top={secondChildTop} width={childWidth} height={childHeight} colorIndex={colorIndex + 1} />
   </div>);
-}
-
-const ImageBgs = () => {
-  const elements = [];
-  const layers = 4;
-
-  const maxWidthFactor = 0.8;
-  const minWidthFactor = 0.8;
-  const maxHeightFactor = 0.8;
-  const minHeightFactor = 0.8;
-  const countFactor = 2;
-
-  let maxWidth = fullImageWidth;
-  let minWidth = maxWidth * .8;
-  let maxHeight = fullImageHeight;
-  let minHeight = maxHeight * .8;
-  let count = 2;
-
-  for (let i = 0; i < layers; i++) {
-    maxWidth *= maxWidthFactor;
-    minWidth = maxWidth * minWidthFactor;
-    maxHeight *= maxHeightFactor;
-    minHeight = maxHeight * minHeightFactor;
-    count *= countFactor;
-    const color = backgroundColors[i % colorCount];
-    elements.push(<ImageBg key={i} minWidth={minWidth} maxWidth={maxWidth} minHeight={minHeight} maxHeight={maxHeight} color={color} count={count} />);
-  }
-
-  return (
-    <div
-      style={{
-        width: `${fullImageWidth}px`,
-        height: `${fullImageHeight}px`,
-        position: "absolute",
-        display: "flex",
-      }}
-    >
-      {elements}
-    </div>
-  );
 }
 
 const ImageLines = () => {
@@ -306,30 +217,6 @@ export const ImageOg = ({ title, dateHuman, keywords }: { title: string, dateHum
     </div>
   );
 };
-
-
-await mkdir("docs/og", { recursive: true });
-
-async function generate() {
-  const image = new ImageResponse(
-    <ImageOg
-      title="Hello World"
-      dateHuman="This is a description"
-      keywords={["keyword1", "keyword2"]}
-    />,
-    {
-      width: 1200,
-      height: 630,
-      
-    }
-  );
-
-  const buffer = Buffer.from(await image.arrayBuffer());
-  await writeFile("docs/og/og-image.png", buffer);
-}
-
-//generate();
-
 
 export const generateDateSocialImage = async (dateEntry: DateEntry) => {
   const imagePath = "docs/" + dateEntry.ogImageUrl;
